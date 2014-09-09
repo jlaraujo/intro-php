164a1f79c6946ecadcfb70359b065f8e316cff74
jlaraujo <jlaa_1993@hotmail.com>
2014-09-09 13:17:06 -0600

    Revert "09-09-2014"
    This reverts commit 3f60fb9f825ba2503d912c09e2a223120bcae85a.

--- ejemplo01/Plantillas/menu.php
+++
@@ -1,8 +0,0 @@
-<nav id="menu">
-	<!--a[href=#]{Enlace$}*5-->
-	<a href="#">Enlace1</a>
-	<a href="#">Enlace2</a>
-	<a href="#">Enlace3</a>
-	<a href="#">Enlace4</a>
-	<a href="#">Enlace5</a>
-</nav>
--- ejemplo01/Plantillas/pie.php
+++
@@ -1,5 +0,0 @@
-<footer id="pie">
-	<p>Tods los Derecos Reservados &copy; 2014</p>
-</footer>
-</body>
-</html>
